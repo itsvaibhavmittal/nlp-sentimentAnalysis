7fd2d860203905f6ddbc35d14fe9e6ecf3600e87
itsvaibhavmittal <itsvaibhavmittal@gmail.com>
2016-11-20 18:48:17 -0600

    feature preparation

--- src/algoStart.py
+++ src/algoStart.py
@@ -22,7 +22,11 @@ wordToDoc = {}
 allWords = []
 stop = set(stopwords.words('english'))
 phrasesScore = {}
+phraseToIndex = {}
+phraseIndex = 0
 tokensScore = {}
+tokenToIndex = {}
+tokenIndex = 0
 phrasesOccurence = {}
 phraseOccurence1 = {}
 phraseOccurence2 = {}
@@ -58,7 +62,7 @@ class Example:
         self.phrases = []
       
 
-def segmentWords( s):
+def segmentWords(s):
     """
      * Splits lines on whitespace for file reading
     """
@@ -70,10 +74,10 @@ def readFile(fileName):
     content = file.read().replace('\n', ' ')
     file.close()
     tokens = nltkUtil.tokenization(content)
-    #print(nltkUtil.posTag(tokens))
-    #tokens = segmentWords(content)
+    # print(nltkUtil.posTag(tokens))
+    # tokens = segmentWords(content)
     for token in tokens:
-        if token ==  "n't":
+        if token == "n't":
             token = "not"
         if token in stop:
             continue
@@ -97,7 +101,7 @@ def tenFoldCrossValidation():
         split = TrainSplit()
         for fName, example in fileToExample.items():
             fileName = example.name
-            if(fileName.startswith('doc_'+str(fold)+'_')):
+            if(fileName.startswith('doc_' + str(fold) + '_')):
                 split.test.append(example)
             else:
                 split.train.append(example)
@@ -113,42 +117,42 @@ def test10Fold():
     total = 0
     print("Training")
     for split in splits:
-        nb =  naiveBayes()
-        #nb = neuralNetwork((5,),1000)
-        #nb = afinn(allWords)
-        #nb = svm()
+        nb = naiveBayes()
+        # nb = neuralNetwork((5,),1000)
+        # nb = afinn(allWords)
+        # nb = svm()
         trainFeatures = []
         trainClasses = []
         testFeatures = []
         testClasses = []
-        print("Training:", count," started" )
+        print("Training:", count, " started")
         for example in split.train:
             trainFeatures.append(example.features)
             trainClasses.append(example.klass)
-        print("Testing:", count," started" )
+        print("Testing:", count, " started")
         for example in split.test:
             testFeatures.append(example.features)
             testClasses.append(example.klass)
         
-        nb.train(trainFeatures,trainClasses )
+        nb.train(trainFeatures, trainClasses)
         nb.test(testFeatures, testClasses)
-        accuracy = nb.getCorrectCount()/ len(testClasses)
-        total = total  + accuracy
+        accuracy = nb.getCorrectCount() / len(testClasses)
+        total = total + accuracy
         print("Correctly Classified:", str(accuracy))
-        #print("Wrongly Classified:",str(nb.getWrongCount()) )
+        # print("Wrongly Classified:",str(nb.getWrongCount()) )
     
-    print("Total accuracy = " , str(total/10))
+    print("Total accuracy = " , str(total / 10))
         
 
 def filterFeatures():
     global idx
     global allWords
     for token, fset in wordToDoc.items():
-        #print(len(wordToDoc[token]))
+        # print(len(wordToDoc[token]))
         if len(wordToDoc[token]) >= 200 and len(wordToDoc[token]) <= 10000:
             allWordsMap[token] = idx
-            idx +=1
-    allWords = [None]*len(allWordsMap)
+            idx += 1
+    allWords = [None] * len(allWordsMap)
     for token, index in allWordsMap.items():
         allWords[index] = token
 
@@ -156,68 +160,74 @@ def filterFeatures():
 def addPhrase(phrase, rating):
     if phrase not in phrasesOccurence:
         phrasesOccurence[phrase] = 0
-    phrasesOccurence[phrase] = phrasesOccurence[phrase] +1
-    if rating ==1:
+    phrasesOccurence[phrase] = phrasesOccurence[phrase] + 1
+    if rating == 1:
         if phrase not in phraseOccurence1:
             phraseOccurence1[phrase] = 0
-        phraseOccurence1[phrase] = phraseOccurence1[phrase] +1
-    elif rating ==2:
+        phraseOccurence1[phrase] = phraseOccurence1[phrase] + 1
+    elif rating == 2:
         if phrase not in phraseOccurence2:
             phraseOccurence2[phrase] = 0
-        phraseOccurence2[phrase] = phraseOccurence2[phrase] +1
-    elif rating ==3:
+        phraseOccurence2[phrase] = phraseOccurence2[phrase] + 1
+    elif rating == 3:
         if phrase not in phraseOccurence3:
             phraseOccurence3[phrase] = 0
-        phraseOccurence3[phrase] = phraseOccurence3[phrase] +1
-    elif rating ==4:
+        phraseOccurence3[phrase] = phraseOccurence3[phrase] + 1
+    elif rating == 4:
         if phrase not in phraseOccurence4:
             phraseOccurence4[phrase] = 0
-        phraseOccurence4[phrase] = phraseOccurence4[phrase] +1
+        phraseOccurence4[phrase] = phraseOccurence4[phrase] + 1
     else:
         if phrase not in phraseOccurence5:
             phraseOccurence5[phrase] = 0
-        phraseOccurence5[phrase] = phraseOccurence5[phrase] +1
+        phraseOccurence5[phrase] = phraseOccurence5[phrase] + 1
 
 def addToken(token, rating, posWords, negWords):
     if token[0] not in posWords and token[0] not in negWords:
         return
     if token not in tokensOccurence:
         tokensOccurence[token] = 0
-    tokensOccurence[token] = tokensOccurence[token] +1
-    if rating ==1:
+    tokensOccurence[token] = tokensOccurence[token] + 1
+    if rating == 1:
         if token not in tokenOccurence1:
             tokenOccurence1[token] = 0
-        tokenOccurence1[token] = tokenOccurence1[token] +1
-    elif rating ==2:
+        tokenOccurence1[token] = tokenOccurence1[token] + 1
+    elif rating == 2:
         if token not in tokenOccurence2:
             tokenOccurence2[token] = 0
-        tokenOccurence2[token] = tokenOccurence2[token] +1
-    elif rating ==3:
+        tokenOccurence2[token] = tokenOccurence2[token] + 1
+    elif rating == 3:
         if token not in tokenOccurence3:
             tokenOccurence3[token] = 0
-        tokenOccurence3[token] = tokenOccurence3[token] +1
-    elif rating ==4:
+        tokenOccurence3[token] = tokenOccurence3[token] + 1
+    elif rating == 4:
         if token not in tokenOccurence4:
             tokenOccurence4[token] = 0
-        tokenOccurence4[token] = tokenOccurence4[token] +1
+        tokenOccurence4[token] = tokenOccurence4[token] + 1
     else:
         if token not in tokenOccurence5:
             tokenOccurence5[token] = 0
-        tokenOccurence5[token] = tokenOccurence5[token] +1
+        tokenOccurence5[token] = tokenOccurence5[token] + 1
         
 
 def filterPhrasesAndTokens():
     print("Filtering Phrases and Tokens")
+    global phraseIndex
+    global tokenIndex
     print("Initial Phrases:", len(phrasesOccurence))
     for phrase, occurence in phrasesOccurence.items():
-        if occurence >=30:
+        if occurence >= 30:
             phrasesScore[phrase] = 0
+            phraseToIndex[phrase] = phraseIndex
+            phraseIndex += 1
     print("Remaining Phrases:", len(phrasesScore))
     
     print("Initial Tokens:", len(tokensOccurence))
     for token, occurence in tokensOccurence.items():
-        if occurence >=30:
+        if occurence >= 30:
             tokensScore[token] = 0
+            tokenToIndex[phrase] = tokenIndex
+            tokenIndex += 1
     print("Remaining Tokens:", len(tokensScore))
 
         
@@ -241,16 +251,24 @@ def preProcessExamples(posWords, negWords):
     
 def preProcessExamplesWithHash():
     print("Preprocesing Examples")
-    numFeatures = len(allWordsMap)
-    print("Features:", numFeatures)
+    numPhrases = len(phraseToIndex)
+    numTokens = len(tokenToIndex)
+    print("Phrases:", numPhrases)
+    print("Tokens:", numTokens)
     for fName, example in fileToExample.items():
+        phrases = example.phrases
+        phraseArray = [0] * numPhrases
         tokens = example.tokens
-        features = [0]*numFeatures
+        tokenArray = [0] * numTokens
+        for phrase in phrases:
+            if phrase in phraseToIndex:
+                phraseArray[phraseToIndex[phrase]] = 1
         for token in tokens:
-            if token in allWordsMap:
-                features[allWordsMap[token]] = 1
-        #stringFeature = ''.join(features)    
-        #example.features = [ctypes.c_size_t(hash(stringFeature)).value]
+            if token in tokenToIndex:
+                tokenArray[tokenToIndex[token]] = 1
+        phraseHash = [ctypes.c_size_t(hash(''.join(phraseArray))).value] 
+        tokenHash = [ctypes.c_size_t(hash(''.join(tokenArray))).value]
+        features = [phraseHash, tokenHash, getAverageScore(example), example.groundTruth, example.label]
         example.features = features
         fileToExample[fName] = example
     print("Examples preprocessed")
@@ -266,7 +284,7 @@ def preprocess(parentDir):
             example.rating = int(fileName.spit("_")[2])
             example.name = fileName
             fileToExample[fname] = example
-    #filterFeatures()
+    # filterFeatures()
     negWords = Processing.getNegativeWords(parentDir)
     posWords = Processing.getPositiveWords(parentDir)
     preProcessExamples(posWords, negWords)
