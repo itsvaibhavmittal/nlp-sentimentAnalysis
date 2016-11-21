2302a5eb271a7a20844af7f0477452b4b4e3d027
itsvaibhavmittal <itsvaibhavmittal@gmail.com>
2016-11-21 00:36:03 -0600

    random forest

---
+++ src/RandomForest.py
@@ -0,0 +1,49 @@
+'''
+Created on Nov 21, 2016
+
+@author: vaibhav
+'''
+from sklearn.ensemble import RandomForestClassifier
+
+class RandomForest:
+    '''
+    This class classifies the data using Random Forest algorithm
+    '''
+
+    def __init__(self, n):
+        '''
+        Constructor
+        '''
+        self.classifier = RandomForestClassifier(n_estimators=n)
+        self.correct = 0
+        self.wrong = 0
+        
+    def train(self, trainingFeatures, trainingClasses):
+        if(len(trainingFeatures) != len(trainingClasses)):
+            print("Length of feature vectors and class vector are different")
+            return
+        
+        X = trainingFeatures
+        Y = trainingClasses
+        self.classifier.fit(X, Y)
+        print("MultinomialNB training complete")
+        
+    def test(self, testFeatures, testClasses):
+        if(len(testFeatures) != len(testClasses)):
+            print("Length of feature vectors and class vector are different")
+            return
+        print("Predicting Classes")
+        predictedClasses = self.classifier.predict(testFeatures)
+        print("Predicted Classes")
+        for i in range(0, len(predictedClasses)):
+            if(predictedClasses[i] == testClasses[i]):
+                self.correct += 1
+            else:
+                self.wrong += 1
+        
+        
+    def getCorrectCount(self):
+        return self.correct
+    
+    def getWrongCount(self):
+        return self.wrong
